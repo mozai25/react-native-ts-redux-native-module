ca5a593a8c1cfc809bb9e9d962e7fb8e28d74fdd
Victor <prolacitn@gmail.com>
2022-03-07 16:51:30 +0100

    redux added

--- test1/index.tsx
+++ test1/index.tsx
@@ -7,8 +7,23 @@ import {LogBox} from 'react-native';
 import App from './App';
 import Info from './Info';
 
+import {Provider} from 'react-redux';
+import {createStore, Store} from 'redux';
+import messagesReducer from './redux/reducers';
+import {
+  addMessage,
+} from './redux/actions';
+
+const store: Store = createStore(messagesReducer);
+
 LogBox.ignoreAllLogs(true);
-Navigation.registerComponent('test1.home', () => App);
+
+Navigation.registerComponent('test1.home', () => props => (
+    <Provider store={store}>
+        <App {...props} />
+    </Provider>
+), () => App);
+
 Navigation.registerComponent('test1.info', () => Info);
 
 Navigation.events().registerAppLaunchedListener(() => {
